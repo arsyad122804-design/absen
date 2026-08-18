84df3b210e2295fd0a770efa9e3d84b8d34d9c39
Arsyad <arsyad@example.com>
2026-08-18 11:46:50 +0700

    Fix-ProfilSaya-useEffect-ReferenceError

--- src/pages/ProfilSaya.jsx
+++ src/pages/ProfilSaya.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useOutletContext, useNavigate } from 'react-router-dom';
 import './ProfilSaya.css';
 import { 
